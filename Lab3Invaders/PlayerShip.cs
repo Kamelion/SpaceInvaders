1dc926fab8a9f099919cb07849d6855b9597ee6e
Andy <alevak4291@gmail.com>
2013-06-28 14:51:39 -0400

    first commit

---
+++ Lab3Invaders/PlayerShip.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Lab3Invaders
+{
+    class PlayerShip
+    {
+        public Point Location { get; private set; }
+        private Bitmap image;
+
+        private bool alive;
+        public bool Alive
+        {
+            get { return alive; }
+            set
+            {
+                alive = value;
+                if (!value)
+                    wait = DateTime.Now;
+            }
+        }
+        private Rectangle boundaries;
+        private DateTime wait;
+        
+        public Rectangle Area
+        {
+            get
+            {
+                return new Rectangle(Location, image.Size);
+            }
+        }
+
+        public PlayerShip(Rectangle boundaries, bool Alive)
+        {
+            this.boundaries = boundaries;
+            this.Alive = Alive;
+            image = Properties.Resources.player;
+            Location = new Point((boundaries.Width / 2) - image.Width / 2, boundaries.Height - 35);
+        }
+
+        public void Draw(Graphics g)
+        {
+            if (Alive)
+            {
+                g.DrawImageUnscaled(image, Location);
+            }
+            else
+            {
+                if (DateTime.Now - wait < TimeSpan.FromSeconds(1))
+                    g.DrawImage(image, Location.X, Location.Y, image.Width, image.Height / 2);
+                else if (DateTime.Now - wait > TimeSpan.FromSeconds(1) &&
+                    DateTime.Now - wait < TimeSpan.FromSeconds(2))
+                    g.DrawImage(image, Location.X, Location.Y, image.Width, image.Height / 4);
+                else if (DateTime.Now - wait > TimeSpan.FromSeconds(2) &&
+                    DateTime.Now - wait < TimeSpan.FromSeconds(3))
+                    g.DrawImage(image, Location.X, Location.Y, image.Width, image.Height / 8);
+                else
+                    Alive = true;
+            }
+        }
+
+        public void Move(Direction direction)
+        {
+            Point newLocation;
+            if (direction == Direction.Left && Location.X >= boundaries.X + 5)
+            {
+                newLocation = new Point(Location.X - 5, Location.Y);
+                Location = newLocation;
+            }
+            else if (direction == Direction.Right && Location.X <= boundaries.Width - 60)
+            {
+                newLocation = new Point(Location.X + 5, Location.Y);
+                Location = newLocation;
+            }
+        }
+    }
+}
---
+++ Lab3Invaders/Stars.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Lab3Invaders
+{
+    class Stars
+    {
+        private struct Star
+        {
+            public Point point;
+            public Pen pen;
+
+            public Star(Point point, Pen pen)
+            {
+                this.point = point;
+                this.pen = pen;
+            }
+        }
+
+        private List<Star> starList;
+
+        public Stars(Random random, Rectangle boundaries)
+        {
+            starList = new List<Star>();
+            for (int i = 0; i < 300; i++)
+            {
+                starList.Add(AddStar(random, boundaries));
+            }
+        }
+
+        public void Draw(Graphics g)
+        {
+            foreach (Star star in starList)
+            {
+                g.DrawLine(star.pen, star.point, new Point(star.point.X + 1, star.point.Y + 1));
+            }
+        }
+
+        public void Twinkle(Random random, Rectangle boundaries)
+        {
+            int index;
+            for (int i = 0; i < 5; i++)
+            {
+                index = random.Next(0, starList.Count);
+                starList.RemoveAt(index);
+                starList.Add(AddStar(random, boundaries));
+            }
+        }
+
+        private Star AddStar(Random random, Rectangle boundaries)
+        {
+            Star starToAdd = new Star();
+            starToAdd.point = new Point(random.Next(0, boundaries.Width),
+                                            random.Next(0, boundaries.Height));
+            starToAdd.pen = RandomPen(random);
+            return starToAdd;
+        }
+
+        private Pen RandomPen(Random random)
+        {
+            int colorNumber = random.Next(0, 4);
+            Color colorType = new Color();
+            switch (colorNumber) 
+            {
+                case 0: colorType = Color.White; break;
+                case 1: colorType = Color.Yellow; break;
+                case 2: colorType = Color.MistyRose; break;
+                default: colorType = Color.LightCyan; break;
+            }
+            Pen randomPen = new Pen(colorType, 1.0F);
+            return randomPen;
+        }
+    }
+}
